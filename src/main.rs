8702e8bfbd2506093cf0c712c287687df16f9d38
Stuart Haidon <serayen.sh@gmail.com>
2019-11-09 15:51:26 +0000

    No need to handle errors in sub-functions during evaluation anymore.

--- src/main.rs
+++ src/main.rs
@@ -117,66 +117,66 @@ fn parse_operations<'a>(input: &[Span<'a>]) -> Vec<Operation<'a>> {
     ops
 }
 
-fn apply_mono_func<'a>(stack: &mut Vec<f64>, t_span: Span<'a>, span: Span<'a>, f: impl Fn(f64) -> f64) -> Result<(), (Span<'a>, Span<'a>, ErrorKind)> {
-    let a = stack.pop().ok_or((t_span, span, ErrorKind::InsufficientStack))?;
+fn apply_mono_func<'a>(stack: &mut Vec<f64>, span: Span<'a>, f: impl Fn(f64) -> f64) -> Result<(), (Span<'a>, ErrorKind)> {
+    let a = stack.pop().ok_or((span, ErrorKind::InsufficientStack))?;
     stack.push(f(a));
 
     Ok(())
 }
 
-fn apply_bi_func<'a>(stack: &mut Vec<f64>, t_span: Span<'a>, span: Span<'a>, f: impl Fn(f64, f64) -> f64) -> Result<(), (Span<'a>, Span<'a>, ErrorKind)> {
+fn apply_bi_func<'a>(stack: &mut Vec<f64>, span: Span<'a>, f: impl Fn(f64, f64) -> f64) -> Result<(), (Span<'a>, ErrorKind)> {
     let (b, a) = stack.pop()
         .and_then(|a| stack.pop().map(|b| (a, b)))
-        .ok_or((t_span, span, ErrorKind::InsufficientStack))?;
+        .ok_or((span, ErrorKind::InsufficientStack))?;
 
     stack.push(f(a, b));
 
     Ok(())
 }
 
-fn evaluate_operations<'a>(ops: &'a [Operation], variables: &HashMap<&str, f64>, functions: &'a HashMap<&'a str, CustomFunction<'a>>, total_span: Span<'a>) -> Result<f64, (Span<'a>, Span<'a>, ErrorKind)> {
+fn evaluate_operations<'a>(ops: &'a [Operation], variables: &HashMap<&str, f64>, functions: &'a HashMap<&'a str, CustomFunction<'a>>, total_span: Span<'a>) -> Result<f64, (Span<'a>, ErrorKind)> {
     let mut stack = Vec::new();
 
     for op in ops {
         match &op.op_type {
             OperationType::Number(num) => stack.push(*num),
-            OperationType::BasicOp(Operator::Add) => apply_bi_func(&mut stack, total_span, op.span, |a, b| a + b)?,
-            OperationType::BasicOp(Operator::Sub) => apply_bi_func(&mut stack, total_span, op.span, |a, b| a - b)?,
-            OperationType::BasicOp(Operator::Mul) => apply_bi_func(&mut stack, total_span, op.span, |a, b| a * b)?,
-            OperationType::BasicOp(Operator::Div) => apply_bi_func(&mut stack, total_span, op.span, |a, b| a / b)?,
-            OperationType::BasicOp(Operator::Pow) => apply_bi_func(&mut stack, total_span, op.span, &f64::powf)?,
-            OperationType::BasicOp(Operator::Mod) => apply_bi_func(&mut stack, total_span, op.span, |a, b| a % b)?,
+            OperationType::BasicOp(Operator::Add) => apply_bi_func(&mut stack, op.span, |a, b| a + b)?,
+            OperationType::BasicOp(Operator::Sub) => apply_bi_func(&mut stack, op.span, |a, b| a - b)?,
+            OperationType::BasicOp(Operator::Mul) => apply_bi_func(&mut stack, op.span, |a, b| a * b)?,
+            OperationType::BasicOp(Operator::Div) => apply_bi_func(&mut stack, op.span, |a, b| a / b)?,
+            OperationType::BasicOp(Operator::Pow) => apply_bi_func(&mut stack, op.span, &f64::powf)?,
+            OperationType::BasicOp(Operator::Mod) => apply_bi_func(&mut stack, op.span, |a, b| a % b)?,
             OperationType::Function(name) => {
                 match name {
-                    &"abs"      => apply_mono_func(&mut stack, total_span, op.span, &f64::abs)?,
-                    &"ceil"     => apply_mono_func(&mut stack, total_span, op.span, &f64::ceil)?,
-                    &"floor"    => apply_mono_func(&mut stack, total_span, op.span, &f64::floor)?,
-                    &"exp"      => apply_mono_func(&mut stack, total_span, op.span, &f64::exp)?,
-                    &"ln"       => apply_mono_func(&mut stack, total_span, op.span, &f64::ln)?,
-                    &"log10"    => apply_mono_func(&mut stack, total_span, op.span, &f64::log10)?,
-                    &"sqrt"     => apply_mono_func(&mut stack, total_span, op.span, &f64::sqrt)?,
-                    &"d2rad"    => apply_mono_func(&mut stack, total_span, op.span, &f64::to_radians)?,
-                    &"r2deg"    => apply_mono_func(&mut stack, total_span, op.span, &f64::to_degrees)?,
-                    &"rnd"      => apply_mono_func(&mut stack, total_span, op.span, &f64::round)?,
+                    &"abs"      => apply_mono_func(&mut stack, op.span, &f64::abs)?,
+                    &"ceil"     => apply_mono_func(&mut stack, op.span, &f64::ceil)?,
+                    &"floor"    => apply_mono_func(&mut stack, op.span, &f64::floor)?,
+                    &"exp"      => apply_mono_func(&mut stack, op.span, &f64::exp)?,
+                    &"ln"       => apply_mono_func(&mut stack, op.span, &f64::ln)?,
+                    &"log10"    => apply_mono_func(&mut stack, op.span, &f64::log10)?,
+                    &"sqrt"     => apply_mono_func(&mut stack, op.span, &f64::sqrt)?,
+                    &"d2rad"    => apply_mono_func(&mut stack, op.span, &f64::to_radians)?,
+                    &"r2deg"    => apply_mono_func(&mut stack, op.span, &f64::to_degrees)?,
+                    &"rnd"      => apply_mono_func(&mut stack, op.span, &f64::round)?,
 
-                    &"log"      => apply_bi_func(&mut stack, total_span, op.span, &f64::log)?,
-                    &"pow"      => apply_bi_func(&mut stack, total_span, op.span, &f64::powf)?,
+                    &"log"      => apply_bi_func(&mut stack, op.span, &f64::log)?,
+                    &"pow"      => apply_bi_func(&mut stack, op.span, &f64::powf)?,
 
-                    &"cos"      => apply_mono_func(&mut stack, total_span, op.span, &f64::cos)?,
-                    &"cosh"     => apply_mono_func(&mut stack, total_span, op.span, &f64::cosh)?,
-                    &"acos"     => apply_mono_func(&mut stack, total_span, op.span, &f64::acos)?,
-                    &"acosh"    => apply_mono_func(&mut stack, total_span, op.span, &f64::acosh)?,
+                    &"cos"      => apply_mono_func(&mut stack, op.span, &f64::cos)?,
+                    &"cosh"     => apply_mono_func(&mut stack, op.span, &f64::cosh)?,
+                    &"acos"     => apply_mono_func(&mut stack, op.span, &f64::acos)?,
+                    &"acosh"    => apply_mono_func(&mut stack, op.span, &f64::acosh)?,
 
-                    &"sin"      => apply_mono_func(&mut stack, total_span, op.span, &f64::sin)?,
-                    &"sinh"     => apply_mono_func(&mut stack, total_span, op.span, &f64::sinh)?,
-                    &"asin"     => apply_mono_func(&mut stack, total_span, op.span, &f64::asin)?,
-                    &"asinh"    => apply_mono_func(&mut stack, total_span, op.span, &f64::asinh)?,
+                    &"sin"      => apply_mono_func(&mut stack, op.span, &f64::sin)?,
+                    &"sinh"     => apply_mono_func(&mut stack, op.span, &f64::sinh)?,
+                    &"asin"     => apply_mono_func(&mut stack, op.span, &f64::asin)?,
+                    &"asinh"    => apply_mono_func(&mut stack, op.span, &f64::asinh)?,
 
-                    &"tan"      => apply_mono_func(&mut stack, total_span, op.span, &f64::tan)?,
-                    &"tanh"     => apply_mono_func(&mut stack, total_span, op.span, &f64::tanh)?,
-                    &"atan"     => apply_mono_func(&mut stack, total_span, op.span, &f64::atan)?,
-                    &"atanh"    => apply_mono_func(&mut stack, total_span, op.span, &f64::atanh)?,
-                    &"atan2"    => apply_bi_func(&mut stack, total_span, op.span, &f64::atan2)?,
+                    &"tan"      => apply_mono_func(&mut stack, op.span, &f64::tan)?,
+                    &"tanh"     => apply_mono_func(&mut stack, op.span, &f64::tanh)?,
+                    &"atan"     => apply_mono_func(&mut stack, op.span, &f64::atan)?,
+                    &"atanh"    => apply_mono_func(&mut stack, op.span, &f64::atanh)?,
+                    &"atan2"    => apply_bi_func(&mut stack, op.span, &f64::atan2)?,
                     &"pi"       => stack.push(std::f64::consts::PI),
                     &"e"        => stack.push(std::f64::consts::E),
 
@@ -187,14 +187,14 @@ fn evaluate_operations<'a>(ops: &'a [Operation], variables: &HashMap<&str, f64>,
                                 let mut fun_vars = variables.clone();
 
                                 for variable_name in fun.variable_names.iter().rev() {
-                                    let val = stack.pop().ok_or((total_span, op.span, ErrorKind::InsufficientStack))?;
+                                    let val = stack.pop().ok_or((op.span, ErrorKind::InsufficientStack))?;
                                     fun_vars.insert(variable_name, val);
                                 }
 
                                 let result = evaluate_operations(&fun.body, &fun_vars, &functions, fun.span)?;
                                 stack.push(result);
                             },
-                            (None, None) => return Err((total_span, op.span, ErrorKind::UnknownFunction)),
+                            (None, None) => return Err((op.span, ErrorKind::UnknownFunction)),
                         }
                     }
                 }
@@ -203,7 +203,7 @@ fn evaluate_operations<'a>(ops: &'a [Operation], variables: &HashMap<&str, f64>,
     }
 
     if stack.len() != 1 {
-        Err((total_span, total_span, ErrorKind::NonEmptyStack))
+        Err((total_span, ErrorKind::NonEmptyStack))
     } else {
         Ok(stack.pop().unwrap())
     }
@@ -257,8 +257,8 @@ fn main() {
                     fun_vars.insert(variable_name, 1.0);
                 }
 
-                if let Err((t_span, span, e)) = evaluate_operations(&body, &fun_vars, &functions, total_span) {
-                    print_error(t_span.value, span, e);
+                if let Err((span, e)) = evaluate_operations(&body, &fun_vars, &functions, total_span) {
+                    print_error(input, span, e);
                 } else {
                     functions.insert(name.value, CustomFunction { span: total_span, body, variable_names });
                 }
@@ -275,7 +275,7 @@ fn main() {
                         println!("Result: {}", result);
                         println!();
                     },
-                    Err((t_span, span, e)) => print_error(t_span.value, span, e),
+                    Err((span, e)) => print_error(input, span, e),
                 }
             },
         };
